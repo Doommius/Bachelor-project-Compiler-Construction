ef18b2935def88c05fe8c4c64c9a26beb2e55c18
Mark Jervelund <mark@jervelund.com>
2018-02-13 17:52:04 +0100

    Program now runs.

--- Scanner_And_Parser/main.c
+++ Scanner_And_Parser/main.c
@@ -1,18 +1,18 @@
+#include <stdio.h>
 #include "tree.h"
 #include "pretty.h"
 #include "y.tab.h"
-#include "lex.yy.c"
 
 
 int lineno;
 
-void yyparse();
-
 EXP *theexpression;
 
-int main()
-{ lineno = 1;
-  yyparse();
-  prettyEXP(theexpression);
-  return 0;
+int main() {
+
+    freopen("input.txt", "r", stdin);
+    lineno = 1;
+    yyparse();
+    prettyEXP(theexpression);
+    return 0;
 }
