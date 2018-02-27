5616aac0d51d83b009e142871fc49af67a22a605
Morten Jæger <murmeldyretj@gmail.com>
2018-02-27 13:42:29 +0100

    started weeder

---
+++ src/modules/weeder/include/weeder.h
@@ -0,0 +1,8 @@
+#ifndef __weeder_h
+#define __weeder_h
+#include "tree.h"
+
+int weeder(body *program);
+
+
+#endif
---
+++ src/modules/weeder/weeder.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include "tree.h"
+
+
+body *theprogram;
+
+
+int weeder(body *program){
+    printf("Fik program til weeder\n");
+}
