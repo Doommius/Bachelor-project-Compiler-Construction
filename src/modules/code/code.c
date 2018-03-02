93ab296596fba3e5069ed05f1176eef9ff80666c
Mark Jervelund <mark@jervelund.com>
2018-03-02 14:42:26 +0100

    added template for first code generation.

--- src/modules/code/code.c
+++ src/modules/code/code.c
@@ -1,36 +1,75 @@
 #include <string.h>
 #include "../parser/include/tree.h"
 
+char eval_statement(statement s) {
 
-char code_or_statement(expression a, expression b){
-    char output     ="(eval(a)";
-    strcat(output,("push  %eax           ;save value of e1 on the stack"));
-    strcat(output,("eval(b)"));
-    strcat(output,("pop   %ecx           ;pop e1 from the stack into ecx"));
-    strcat(output,("orl   %ecx, %eax     ;compute e1 | e2, set ZF"));
-    strcat(output,("movl  $0, %eax       ;zero out EAX without changing ZF"));
-    strcat(output,("setne %al            ;set AL register (the lower byte of EAX) to 1 iff e1 | e2 != 0"));
-    return output;
+    switch (s.kind) {
+    case statement_WRITE:
+    case statement_ALLOCATE:
+    case statement_ALLOCATE_LENGTH:
+    case statement_ASSIGNMENT:
+    case statement_IF:
+    case statement_IF_ELSE:
+    case statement_WHILE:
+    case statement_LIST:
+    }
+    return " ";
 }
 
-char code_and_statement(expression a, expression b){
-    char output     = "<CODE FOR e1 GOES HERE>";
-    strcat(output,("push  %eax            ;save value of e1 on the stack"));
-    strcat(output,("        <CODE FOR e2 GOES HERE>"));
-    strcat(output,("pop   %ecx            ;pop e1 from the stack into ECX"));
-    strcat(output,("; Step 1: SET CL = 1 iff e1 != 0"));
-    strcat(output,("cmpl  $0, %ecx        ;compare e1 to 0"));
-    strcat(output,("setne %cl             ;set CL to 1 iff e1 != 0"));
-    strcat(output,("        ; Step 2: SET AL = 1 iff e2 != 0"));
-    strcat(output,("cmpl  $0, %eax        ;compare e2 to 0"));
-    strcat(output,("movl  $0, %eax        ;zero EAX register before storing result"));
-    strcat(output,("setne %al             ;set AL to 1 iff e2 != 0"));
-    strcat(output,("        ; Step 3: compute al & cl"));
-    strcat(output,("andb  %cl, %al        ;store AL & CL in AL"));
-    return output;
-}
+char eval_expression(expression e) {
 
-char code_GQ_statement(expression a, expression b){
+    switch (e.kind) {
+    case exp_PLUS:
+    case exp_MIN:
+    case exp_MULT:
+    case exp_DIV:
+    case exp_EQ:
+    case exp_NEQ:
+    case exp_GT:
+    case exp_LT:
+    case exp_GEQ:
+    case exp_LEQ:
+    case exp_AND:
+        return code_and_expression(e);
+        break;
+    case exp_OR:
+        return code_or_expression(e);
+        break;
+    case exp_TERM:
+    }
+    return " ";
+}
+char code_GQ_statement(expression a, expression b) {
     char output = "<CODE FOR e1 GOES HERE>";
     return output;
 }
+
+
+char code_or_expression(expression e) {
+    char output = eval_expression(e.val.ops.left);
+    strcat(output, ("push  %eax           ;save value of e1 on the stack"));
+    strcat(output, eval_expression(e.val.ops.left));
+    strcat(output, ("pop   %ecx           ;pop e1 from the stack into ecx"));
+    strcat(output, ("orl   %ecx, %eax     ;compute e1 | e2, set ZF"));
+    strcat(output, ("movl  $0, %eax       ;zero out EAX without changing ZF"));
+    strcat(output, ("setne %al            ;set AL register (the lower byte of EAX) to 1 iff e1 | e2 != 0"));
+    return output;
+}
+
+char code_and_expression(expression e) {
+    char output = eval_expression(e.val.ops.left);
+    strcat(output, ("push  %eax            ;save value of e1 on the stack"));
+    strcat(output, (eval_expression(e.val.ops.right)));
+    strcat(output, ("pop   %ecx            ;pop e1 from the stack into ECX"));
+    strcat(output, ("; Step 1: SET CL = 1 iff e1 != 0"));
+    strcat(output, ("cmpl  $0, %ecx        ;compare e1 to 0"));
+    strcat(output, ("setne %cl             ;set CL to 1 iff e1 != 0"));
+    strcat(output, ("        ; Step 2: SET AL = 1 iff e2 != 0"));
+    strcat(output, ("cmpl  $0, %eax        ;compare e2 to 0"));
+    strcat(output, ("movl  $0, %eax        ;zero EAX register before storing result"));
+    strcat(output, ("setne %al             ;set AL to 1 iff e2 != 0"));
+    strcat(output, ("        ; Step 3: compute al & cl"));
+    strcat(output, ("andb  %cl, %al        ;store AL & CL in AL"));
+    return output;
+}
+
