a3abfd705988eb550895157ab1fb1e833cb42334
Troels Blicher Petersen <thesleort@gmail.com>
2018-03-06 16:47:46 +0100

    added missing headers

--- src/modules/code/include/code.h
+++ src/modules/code/include/code.h
@@ -4,17 +4,27 @@
 #include "tree.h"
 #include "linked_list.h"
 
-void eval_statement(statement *s);
-
-void eval_expression(expression *e);
-
-void code_GEQ_statement(expression *e);
-
 void code_or_expression(expression *e);
 
 void code_and_expression(expression *e);
 
 void code_general_expression(expression *e);
+
+void code_GEQ_statement(expression *e);
+
+void code_LEQ_statement(expression *e);
+
+void code_LT_statement(expression *e);
+
+void code_GT_statement(expression *e);
+
+void code_NEQ_statement(expression *e);
+
+void code_EQ_statement(expression *e);
+
+void eval_expression(expression *e);
+
+void eval_statement(statement *s);
 
 linked_list *buildcode(statement_list input);
 
