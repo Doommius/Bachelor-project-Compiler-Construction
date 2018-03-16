49a13eebe9f52ff789f7bd886d08cebef8150ea3
Troels Blicher Petersen <thesleort@gmail.com>
2018-03-16 10:01:51 +0100

    updated test.sh

--- test.sh
+++ test.sh
@@ -6,22 +6,23 @@ make
 echo "+-------------+"
 echo "|    Tests    |"
 echo "+-------------+"
-for file in ./tests/*.src
+for file in ./tests/*/*.src
 do
 	echo "+--------------------"
 	echo "| TEST: "$file
 	echo "+--------------------"
 	./compiler $file
+	echo " "
 done
 
-echo "+-------------+"
-echo "|  Examples   |"
-echo "+-------------+"
-for file in ./examples/*.src
-do
-	echo "+--------------------"
-	echo "| EXAMPLE: "$file
-	echo "+--------------------"
-	./compiler $file
-done
+# echo "+-------------+"
+# echo "|  Examples   |"
+# echo "+-------------+"
+# for file in ./examples/*.src
+# do
+# 	echo "+--------------------"
+# 	echo "| EXAMPLE: "$file
+# 	echo "+--------------------"
+# 	./compiler $file
+# done
 echo "done"
